46a06e2a2e8cb19198f8f6818e7adb55881f5d1d
sharkfacekilla <sharkfacekilla@gmail.com>
2026-01-28 15:01:48 -0800

    testing

--- app/routes/event/components/eventCard.tsx
+++ app/routes/event/components/eventCard.tsx
@@ -4,14 +4,15 @@ import { Card, CardContent } from "~/components/ui/card";
 
 
 export default function EventCard({event} : {event: any}) {
-  return (
+console.log(event);
+    return (
                 <Link to={`/event/${event.slug}`}>
     <Card>
         <CardContent></CardContent>
         <div className="flex flex-col items-center">
             <div className="mb-4">
                 <h1>{event.name}</h1>
-                
+                {event.slug}
                 
                 
                 
